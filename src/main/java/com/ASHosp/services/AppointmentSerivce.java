aec71a7498a274fcd48345ad52e5e2026f7ea42e
ashish200214 <gawaliashish2002@gmail.com>
2025-06-20 17:07:00 +0530

    log

--- src/main/java/com/ASHosp/services/AppointmentSerivce.java
+++ src/main/java/com/ASHosp/services/AppointmentSerivce.java
@@ -2,7 +2,6 @@ package com.ASHosp.services;
 
 import java.util.List;
 
-import com.ASHosp.entity.Patient;
 import com.ASHosp.entity.Search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
